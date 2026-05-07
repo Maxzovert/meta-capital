dceac9ab3667016e513f88c30ae5d40c54ae2ec4
Maxzovert <95abdullah99@gmail.com>
2026-05-07 14:35:56 +0530

    Remove ScrollToTopButton import from MainLayout and retain MutualFundDisclaimer component for user acknowledgment.

--- src/Components/layout/MainLayout.jsx
+++ src/Components/layout/MainLayout.jsx
@@ -1,11 +1,8 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
-<<<<<<< HEAD
 import MutualFundDisclaimer from '@/components/common/MutualFundDisclaimer'
-=======
 import ScrollToTopButton from '@/components/layout/ScrollToTopButton'
->>>>>>> e2d2c1af9bb2792c024b9c3f82754cd8d48c01ab
 
 export default function MainLayout() {
   return (
